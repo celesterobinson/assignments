4fb26dd4c34112a652488da842cb12245268cb1e
V School Student <celestekrobinson@gmail.com>
2017-12-09 15:27:45 -0700

    Travel form now has more CSS interactive properties, fixed the gender: on return. Now returns gender. Was missing the property value in the HTML.

--- exercises/travel-form/main.js
+++ exercises/travel-form/main.js
@@ -3,27 +3,33 @@ var doStuff = function() {
     var lastName = document.getElementsByName('lastName')[0].value;
     var age = document.getElementsByName('age')[0].value;
     var gender = document.querySelector('input[name=gender]:checked').value;
+    console.log(gender);
     var destination = document.getElementsByName('destination')[0].value;
     var dietary;
-    var vege = document.getElementsByName('dietary')[0];
-    var lact = document.getElementsByName('dietary')[1];
-    var kosher = document.getElementsByName('dietary')[2];    
-    if ((vege.checked && lact.checked) && kosher.checked) {
+    var vege = document.getElementsByName('dietary')[0].checked;
+    var lact = document.getElementsByName('dietary')[1].checked;
+    var kosher = document.getElementsByName('dietary')[2].checked;    
+    if ((vege && lact) && kosher) {
         dietary = 'Vegetarian, Lactose Free, Kosher';
-    } else if (vege.checked && lact.checked) {
+    } else if (vege && lact) {
         dietary = 'Vegetarian, Lactose Free';
-    } else if (vege.checked && kosher.checked) {
+    } else if (vege && kosher) {
         dietary = 'Vegetarian, Kosher';
-    } else if (lact.checked && kosher.checked) {
+    } else if (lact && kosher) {
         dietary = 'Lactose, Kosher';
-    } else if (vege.checked) {
+    } else if (vege) {
         dietary = 'Vegetarian';
-    } else if (lact.checked) {
+    } else if (lact) {
         dietary = 'Lactose Free';
-    } else if (kosher.checked) {
+    } else if (kosher) {
         dietary = 'Kosher';
     } else {
         dietary = "None";
     }
-    alert("First name: " + firstName + "\n" + "Last name: " + lastName + "\n" + "Age: " + age + "\n" + "Gender: " + gender + "\n" + "Destination: " + destination + "\n" + "Dietary Restrictions: " + dietary);
+    alert("First name: " + firstName + 
+    "\n" + "Last name: " + lastName + 
+    "\n" + "Age: " + age + 
+    "\n" + "Gender: " + gender + 
+    "\n" + "Destination: " + destination + 
+    "\n" + "Dietary Restrictions: " + dietary);
 }
