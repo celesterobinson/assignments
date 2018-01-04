f26d24585653bdbdefcd491bbcde33e4e7b9e67b
V School Student <celestekrobinson@gmail.com>
2018-01-04 11:43:43 -0700

    Completed game

--- exercises/princess-in-another-castle/index.js
+++ exercises/princess-in-another-castle/index.js
@@ -2,7 +2,7 @@ class Player {
     constructor(name) {
         this.name = name;
         this.totalCoins = 0;
-        this.status = "Big";
+        this.status = "Small";
         this.star = false;
         this.gameActive = true;
     }
@@ -40,9 +40,11 @@ class Player {
         switch(this.status) {
             case "Small":
                 this.status = "Powered Up";
+                this.star = true;
                 break;
             case "Big":
                 this.status = "Powered Up";
+                this.star = true;
                 break;
         }
         return this.status;
@@ -55,7 +57,30 @@ class Player {
     print(name) {
         return `Name: ${this.name} \nStatus: ${this.status} \nStar: ${this.star} \nTotal Coins: ${this.totalCoins} \nGame Active: ${this.gameActive}`;
     }
+
+    randomRange() {
+        let randomNum = Math.floor(Math.random() * 3);
+        switch(randomNum) {
+            case 0:
+                return this.gotHit();
+                break;
+            case 1:
+                return this.gotPowerup();
+                break;
+            case 2:
+                return this.addCoin();
+                break;
+        }
+    }
 }
 
-let player = new Player("Celeste");
-console.log(player.print("Celeste"));
+function play() {
+    let player = new Player("Mario");
+    player.randomRange();
+    player.randomRange();
+    player.randomRange();
+    return player.print(player);
+}
+
+console.log(play());
+
