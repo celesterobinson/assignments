31041940ba39c7ce41ac4c40a180e38067311719
V School Student <celestekrobinson@gmail.com>
2018-01-09 15:04:18 -0700

    Complete

---
+++ exercises/name-entry/src/App/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import NameList from "./NameList";
+
+function App() {
+    return (
+        <div>
+            <NameList></NameList>
+        </div>
+    )
+}
+
+export default App
